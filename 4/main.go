318802b382219ba970a40642046e64c3e3f1676b
Alexander Rubtsov <holoholo239@gmail.com>
2024-12-06 13:48:27 +0300

    4_2

--- 4/main.go
+++ 4/main.go
@@ -11,7 +11,6 @@ const inputFileName = "input"
 
 type Field struct {
 	f [][]rune
-	d map[Coord][]Coord
 }
 
 type Coord struct {
@@ -41,17 +40,6 @@ func main() {
 
 	field := Field{
 		f: make([][]rune, 0),
-		d: map[Coord][]Coord{
-			ALL:       {UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT},
-			UP:        {UPLEFT, UP, UPRIGHT},
-			DOWN:      {DOWNLEFT, DOWN, DOWNRIGHT},
-			LEFT:      {UPLEFT, LEFT, DOWNLEFT},
-			RIGHT:     {UPRIGHT, RIGHT, DOWNRIGHT},
-			UPLEFT:    {LEFT, UPLEFT, UP},
-			UPRIGHT:   {RIGHT, UPRIGHT, UP},
-			DOWNLEFT:  {DOWN, DOWNLEFT, LEFT},
-			DOWNRIGHT: {DOWN, DOWNRIGHT, RIGHT},
-		},
 	}
 
 	for scanner.Scan() {
@@ -60,13 +48,68 @@ func main() {
 		field.f = append(field.f, []rune(line))
 	}
 
-	res1 := field.part1()
+	//res1 := field.part1()
+	res2 := field.part2()
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(res1)
+	//fmt.Println(res1)
+	fmt.Println(res2)
+}
+
+func (f *Field) part2() int {
+	res := 0
+
+	for y, row := range f.f {
+		if y == 0 || y == len(f.f)-1 {
+			continue
+		}
+		for x := range row {
+			if x == 0 || x == len(row)-1 {
+				continue
+			}
+
+			if f.f[y][x] == 'A' &&
+				f.checkX(Coord{x: x, y: y}) {
+				res++
+			}
+		}
+	}
+
+	return res
+}
+
+func (f *Field) checkX(c Coord) bool {
+	/*
+		     1    2    3    4
+			M.S  S.S  S.M  M.M
+			.A.  .A.  .A.  .A.
+			M.S  M.M  S.M  S.S
+	*/
+
+	case1 := f.f[c.y-1][c.x-1] == 'M' && // up left
+		f.f[c.y+1][c.x-1] == 'M' && // down left
+		f.f[c.y-1][c.x+1] == 'S' && //up right
+		f.f[c.y+1][c.x+1] == 'S' //down right
+
+	case2 := f.f[c.y-1][c.x-1] == 'S' && // up left
+		f.f[c.y+1][c.x-1] == 'M' && // down left
+		f.f[c.y-1][c.x+1] == 'S' && //up right
+		f.f[c.y+1][c.x+1] == 'M' //down right
+
+	case3 := f.f[c.y-1][c.x-1] == 'S' && // up left
+		f.f[c.y+1][c.x-1] == 'S' && // down left
+		f.f[c.y-1][c.x+1] == 'M' && //up right
+		f.f[c.y+1][c.x+1] == 'M' //down right
+
+	case4 := f.f[c.y-1][c.x-1] == 'M' && // up left
+		f.f[c.y+1][c.x-1] == 'S' && // down left
+		f.f[c.y-1][c.x+1] == 'M' && //up right
+		f.f[c.y+1][c.x+1] == 'S' //down right
+
+	return case1 || case2 || case3 || case4
 }
 
 func (f *Field) part1() int {
@@ -112,22 +155,17 @@ func (f *Field) countXMAS(c Coord) int {
 func (f *Field) searchNear(r rune, at, dir Coord) []Coord {
 	res := make([]Coord, 0)
 
+	var ds []Coord
+
 	if dir == ALL {
-		ds := f.d[dir]
-
-		for _, d := range ds {
-			x := at.x + d.x
-			y := at.y + d.y
-
-			if x >= 0 && x < len(f.f[0]) && y >= 0 && y < len(f.f) {
-				if f.f[y][x] == r {
-					res = append(res, Coord{x: x, y: y})
-				}
-			}
-		}
+		ds = []Coord{UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT}
 	} else {
-		x := at.x + dir.x
-		y := at.y + dir.y
+		ds = []Coord{dir}
+	}
+
+	for _, d := range ds {
+		x := at.x + d.x
+		y := at.y + d.y
 
 		if x >= 0 && x < len(f.f[0]) && y >= 0 && y < len(f.f) {
 			if f.f[y][x] == r {
